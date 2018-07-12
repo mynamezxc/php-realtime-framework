262a1071709df04c80d5a0bf73b1ceb7c81520e6
Nguyen Hoang <nguyenhoangnguyen.ltmt15th@gmail.com>
2018-07-12 15:11:47 +0700

    v1.7.1

--- websocket/on.php
+++ websocket/on.php
@@ -7,14 +7,14 @@
         if($config->remote_server == "windows") {
             if($config->php_exe_path !== "") {
                 //Disabled windows
-                die("Browser is not yet supported. Try again later");
+                die("Operating system is not yet supported. Try again later");
                 $command_script = "nohup {$config->php_exe_path} {$config->server_file_name} logs/nohup.log 2>&1 &";
                 $a = shell_exec ($command_script);
                 $command_script = "echo $! > pids/pid_proccess.txt";
                 shell_exec ($command_script);
             } else {
                 //Disabled windows
-                die("Browser is not yet supported. Try again later");
+                die("Operating system is not yet supported. Try again later");
                 $command_script = "where php.exe";
                 $php_exe_path = trim(preg_replace('/\s\s+/', ' ', shell_exec ($command_script)));
                 $command_script = "nohup {$php_exe_path} {$config->server_file_name} logs/nohup.log 2>&1 &";
